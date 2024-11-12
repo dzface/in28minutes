e4a4a5a69b51d22fabae77efce737453df5eea43
dzface <dzface031@gmail.com>
2024-11-12 15:40:46 +0900

    Security 기본 아이디 비번 설정

---
+++ src/main/java/com/example/in28min/auth/SpringSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.in28min.auth;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsMaager(){
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("in28minutes")
+                .password("dummy")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
