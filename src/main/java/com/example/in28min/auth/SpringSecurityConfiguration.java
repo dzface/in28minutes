85419188d54a3db943582498072c985a5f58f848
dzface <dzface031@gmail.com>
2024-11-12 20:41:27 +0900

    H2 연결 csrf, frameoption 비활성화

--- src/main/java/com/example/in28min/auth/SpringSecurityConfiguration.java
+++ src/main/java/com/example/in28min/auth/SpringSecurityConfiguration.java
@@ -3,13 +3,18 @@ package com.example.in28min.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.web.servlet.function.RequestPredicates.headers;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -37,5 +42,20 @@ public class SpringSecurityConfiguration {
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    // Security를 초기설정만 했을 때 작동되는 기능
+    // 1. 모든 URL 보호
+    // 2. 비인가 이용자, 비인가주소 요청은 기본로그인 창으로 요청
+
+    // H2 사용 시 CSRF 허용 Frames를 허용 해주어야함
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated())
+                .formLogin(form -> form.defaultSuccessUrl("/",true))
+                .csrf(csrf-> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
     }
 }
